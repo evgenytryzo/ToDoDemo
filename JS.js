9654810dec01f750a615e355a89aec140a646a12
tryzo <g0022@inbox.ru>
2023-01-30 20:22:06 +0300

    first commit

---
+++ JS.js
@@ -0,0 +1,49 @@
+{
+    //Есть косяк по "hovered". Надо его будет исправить.
+
+    const item = document.querySelector('.item');
+    const placeholders = document.querySelectorAll('.placeholder')
+
+    item.addEventListener('dragstart', dragstart);
+    item.addEventListener('dragend', dragend);
+
+    for (const placeholder of placeholders) {
+        placeholder.addEventListener('dragover', dragover)
+        placeholder.addEventListener('dragenter', dragenter)
+        placeholder.addEventListener('dragLeave',
+            dragLeave)
+        placeholder.addEventListener('drop', drop)
+    }
+
+    function dragstart(event) {
+        event.target.classList.add('hold')
+        setTimeout(() => event.target.classList.add('hide'), 0)
+
+    }
+
+    function dragend(event) {
+        event.target.className = 'item'
+
+    }
+
+    function dragover(event) {
+        event.preventDefault()
+
+    }
+
+    function dragLeave(event) {
+        event.target.classList.remove('hovered')
+        console.log('rem')
+    }
+
+    function dragenter(event) {
+        event.target.classList.add('hovered')
+        console.log('ent')
+    }
+
+
+    function drop(event) {
+        event.target.classList.remove('hovered')
+        event.target.append(item)
+    }
+}
